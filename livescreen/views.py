94fa2b834fd1a28df715979ca2e07107bcadfb8f
wwangwe <wangwetymothy@gmail.com>
2024-01-16 20:55:41 +0300

    Use timezone for accurate time

--- livescreen/views.py
+++ livescreen/views.py
@@ -1,11 +1,14 @@
 # example/views.py
+import pytz
+import requests
 from datetime import datetime
 from django.shortcuts import render
-import requests
 
 def index(request):
     apiKey = '84bedfaec65643489e7114518241501'
     url = 'https://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q=Nairobi&aqi=no'
+    timezone = pytz.timezone('Africa/Nairobi')
+    now = datetime.now(timezone)
     
     response = requests.get(url)
     data = response.json()
@@ -14,8 +17,8 @@ def index(request):
         'temp': data['current']['temp_c'],
         'condition': data['current']['condition']['text'],
         'icon': data['current']['condition']['icon'],
-        'time': datetime.now().strftime('%H:%M'),
-        'date': datetime.now().strftime('%Y-%m-%d'),
-        'day': datetime.now().strftime('%A'),
+        'time': now.strftime('%H:%M'),
+        'date': now.strftime('%Y-%m-%d'),
+        'day': now.strftime('%A'),
     }
     return  render(request, 'livescreen/index.html', context)
