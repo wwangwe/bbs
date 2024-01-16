8d445d6c8459bc3e60cd45c255171919ed0607ad
wwangwe <wangwetymothy@gmail.com>
2024-01-16 12:00:13 +0300

    Manually turn off debug mode

--- bbs/settings.py
+++ bbs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=dfiiko-i5_zt6twkw$cb!h(yz&0**9u9f%k-uq180_u2n%mei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh', '.kesholabs.com']
 
